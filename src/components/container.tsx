21b8b7ada1c793dec719e045b22d72fb23d402c3
nirjhorsaha <nirjhor.badhan25@gmail.com>
2025-01-09 17:19:09 +0600

    implement navbar

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,15 +1,15 @@
-// import { ReactNode } from "react";
+import { ReactNode } from "react";
 
-// export default function Container({
-//     className,
-//     children,
-// }: {
-//     className?: string;
-//     children: ReactNode;
-// }) {
-//     return (
-//         <div className={`mx-auto max-w-[2520px] xl:px-20 md:px-10 sm:px-2 p-4 ${className}`}>
-//             {children}
-//         </div>
-//     );
-// }
+export default function Container({
+    className,
+    children,
+}: {
+    className?: string;
+    children: ReactNode;
+}) {
+    return (
+        <div className={`mx-auto max-w-[2520px] xl:px-20 md:px-10 sm:px-2 p-4 ${className}`}>
+            {children}
+        </div>
+    );
+}
