2abc843e7101b8066f4544d85b9eede5521ec161
nirjhor saha <nirjhor.badhan25@gmail.com>
2025-01-11 00:33:50 +0600

    formate code

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -5,49 +5,52 @@ import { formatPostDate } from "../utils/DateFormate";
 import useReadMoreStore from "../utils/zustandStore";
 
 interface BlogCardProps {
-  post: Post;
+    post: Post;
 }
 
 const BlogCard = ({ post }: BlogCardProps) => {
-  const formattedDate = formatPostDate(post?.publishedAt);
-  const increment = useReadMoreStore((state) => state.increment);
+    const formattedDate = formatPostDate(post?.publishedAt);
+    const increment = useReadMoreStore((state) => state.increment);
 
-  const truncateContent = (content: string, charLimit: number) => {
-    if (content.length > charLimit) {
-      return content.slice(0, charLimit) + "...";
-    }
-    return content
-  }
+    const truncateContent = (content: string, charLimit: number) => {
+        if (content.length > charLimit) {
+            return content.slice(0, charLimit) + "...";
+        }
+        return content;
+    };
 
-  return (
-    <div className="group border border-gray-300 rounded-2xl bg-[#F7F9F9]">
-      <div className="flex items-center">
-        <img
-          src={post?.image}
-          alt="blogs tailwind section"
-          className="rounded-t-2xl size-full object-cover"
-        />
-      </div>
-      <div className="p-4 lg:p-6 transition-all duration-300 rounded-b-2xl ">
-        <div className="flex justify-between font-medium items-center">
-          <span className="flex mb-3 gap-2 items-center">
-            <FaRegCalendarAlt size={18} />
-            {formattedDate}
-          </span>
-          <p className="bg-white px-4 py-1 rounded-md">{post?.slug}</p>
+    return (
+        <div className="group border border-gray-300 rounded-2xl bg-[#F7F9F9]">
+            <div className="flex items-center">
+                <img
+                    src={post?.image}
+                    alt="blogs image"
+                    className="rounded-t-2xl size-full object-cover"
+                />
+            </div>
+            <div className="p-4 lg:p-6 transition-all duration-300 rounded-b-2xl ">
+                <div className="flex justify-between font-medium items-center">
+                    <span className="flex mb-3 gap-2 items-center">
+                        <FaRegCalendarAlt size={18} />
+                        {formattedDate}
+                    </span>
+                    <p className="bg-white px-4 py-1 rounded-md">{post?.slug}</p>
+                </div>
+                <h4 className="text-3xl text-gray-900 font-medium leading-9 my-3">{post?.title}</h4>
+                <p className="text-gray-500 leading-6">
+                    {truncateContent(post?.content, 200)}
+                    <Link to={`/posts/${post?.id}`}>
+                        <button
+                            className="text-[#206CE5] font-semibold"
+                            onClick={() => increment()}
+                        >
+                            Read more
+                        </button>
+                    </Link>
+                </p>
+            </div>
         </div>
-        <h4 className="text-3xl text-gray-900 font-medium leading-9 my-3">
-          {post?.title}
-        </h4>
-        <p className="text-gray-500 leading-6">
-          {truncateContent(post?.content, 200)}
-          <Link to={`/posts/${post.id}`}>
-            <button className="text-[#206CE5] font-semibold" onClick={() => increment()}>Read more</button>
-          </Link>
-        </p>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default BlogCard;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,27 +4,25 @@ import { Link } from "react-router-dom";
 import useReadMoreStore from "../utils/zustandStore";
 
 const Navbar = () => {
-    const clickCount = useReadMoreStore((state) => state.clickCount); // clickCount values from the store
+    const clickCount = useReadMoreStore((state) => state.clickCount); // clickCount values from store
 
     return (
         <nav className="fixed bg-white py-4 w-full z-20 top-0 start-0 border-b border-[#DBD7D7]">
             <div className="mx-auto  xl:px-20 md:px-10 px-4 flex items-center justify-between">
-                <Link to="/" aria-label="Go to Home">
+                <Link to="/" aria-label="Home">
                     <img src="/SIT-logo.png" className="h-16 cursor-pointer" alt="SynesisIT Logo" />
                 </Link>
                 {/* icons for small devices */}
                 <div className="flex items-center gap-2 md:hidden">
                     <button
                         aria-label="Bell notifications"
-                        className="text-gray-800 p-1 rounded-full flex justify-center items-center hover:bg-gray-100 transition"
-                    >
+                        className="text-gray-800 p-1 rounded-full flex justify-center items-center hover:bg-gray-100 transition">
                         <PiBellRingingBold size={24} />
                         <sup className="text-sm font-bold">{clickCount}</sup>
                     </button>
                     <button
                         aria-label="Login"
-                        className="text-gray-800  rounded-full flex justify-center items-center hover:bg-gray-100 transition"
-                    >
+                        className="text-gray-800  rounded-full flex justify-center items-center hover:bg-gray-100 transition">
                         <LuLogIn size={28} />
                     </button>
                 </div>
@@ -33,22 +31,21 @@ const Navbar = () => {
                 <div className="hidden md:flex items-center gap-4">
                     <button
                         aria-label="Bell notifications"
-                        className="bg-[#D9D9D933] text-gray-800 font-semibold py-2 px-4 rounded-md flex justify-center items-center hover:bg-gray-100 transition"
-                    >
+                        className="bg-[#D9D9D933] text-gray-800 font-semibold py-2 px-4 
+                        rounded-md flex justify-center items-center hover:bg-gray-100 transition">
                         Bell Counter <PiBellRingingBold size={20} className="ml-2" />
                         <sup>{clickCount}</sup>
                     </button>
                     <div className="flex gap-4">
                         <button
                             type="button"
-                            className="bg-[#E3E3E3] text-gray-800 px-12 rounded-md flex justify-center items-center hover:bg-gray-300 transition border border-[#767676]"
-                        >
+                            className="bg-[#E3E3E3] text-gray-800 px-12 rounded-md flex justify-center items-center hover:bg-gray-300 transition border 
+                            border-[#767676]">
                             Sign in
                         </button>
                         <button
                             type="button"
-                            className="bg-black text-white  px-12 rounded-md flex justify-center items-center hover:bg-gray-800 transition"
-                        >
+                            className="bg-black text-white px-12 rounded-md flex justify-center items-center hover:bg-gray-800 transition">
                             Register
                         </button>
                     </div>
